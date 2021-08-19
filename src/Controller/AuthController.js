9adfd923f1e2f6173f7e084a353b396dfef42a99
José Paes <jose.paes.alves@gmail.com>
2021-08-19 20:26:25 -0300

    Criação do arquivo de autenticação

---
+++ src/Controller/AuthController.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../Models/User');
+
+module.exports = {
+  async login(req, res) {
+    const { email, senha } = req.body;
+    try {
+      //validando usuário e senha
+      const user = await User.findOne({ email }).select('+senha');
+      if (!user) {
+        return res.status(401).send({ message: 'Usuário e/ou senha inválidos' });
+      }
+      if (!bcrypt.compareSync(senha, user.senha)) {
+        return res.status(401).send({ message: 'Usuário e/ou senha inválidos' });
+      }
+      //definindo o último login do usuario
+      user.ultimo_login = Date();
+      await user.save();
+
+      const payload = { _id: User._id };
+      const token = jwt.sign(payload, process.env.KEY_JWT, { expiresIn: 60 * 30 });
+      user.token = token;
+      res.set('authorization', token);
+      await user.save();
+      return res.status(200).json(user);
+    } catch (error) {
+      return res.status(500).send({ message: 'Erro desconhecido' });
+    }
+  },
+};
